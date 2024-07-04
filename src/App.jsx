45293ddafd98a3e2502e0086d0373780d6b78bda
Yash12325 <yashg12325@gmail.com>
2024-07-04 15:23:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import HeroSection from './components/HeroSection.jsx'
+import Navbar from './components/navbar.jsx'
+
+const App=() => {
+  return (
+    <div >
+     <Navbar/>
+     <HeroSection/>
+    </div>
+  )
+}
+
+export default App
+  
